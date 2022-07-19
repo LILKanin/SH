6935a4c3b4db7c1441e029bebfeea796aa07cf06
LILKanin <menpak59@gmail.com>
2022-07-20 01:40:18 +0300

    2 загрузка файлов

---
+++ js.js
@@ -0,0 +1,19 @@
+/* Открыть, когда кто-то нажимает на элемент span */
+function openNav(test) {
+  document.getElementById("myNav").style.width = "50%";
+  document.querySelector(".class1").innerHTML = `ЗДАРОВА егор`;
+}
+
+/* Закрыть, когда кто-то нажимает на символ "x" внутри наложения */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+window.addEventListener("load", () => {
+  let name = prompt(
+    "Как вас зовут",
+    ""
+  );
+  document.querySelector(".class1").innerHTML = `ЗДАРОВА ${name}`;
+});
+window.addEventListener("")
